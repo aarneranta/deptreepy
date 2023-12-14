80d2115ff0822e11ab26248f2ea3c5784246001e
aarneranta <aarne@chalmers.se>
2023-12-14 17:51:00 +0100

    operations, pipes, and decorators started

--- operations.py
+++ operations.py
@@ -1,8 +1,10 @@
 # operations that transform dependency structures and can be piped if types match
 
+import sys
 from dataclasses import dataclass
 from typing import Iterable, Callable
 from trees import *
+from patterns import *
 
 
 @dataclass
@@ -10,6 +12,7 @@ class Operation:
     oper: Callable
     argtype: type
     valtype: type
+    name: str
     doc: str
     
     def __call__(self, arg):
@@ -18,72 +21,114 @@ class Operation:
         return doc
 
 
+def operation(f: Callable) -> Operation:
+    if len(anns := f.__annotations__) == 2 and 'return' in anns:
+        return Operation(
+                f,
+                list(anns.values())[0],
+                anns['return'],
+                f.__name__,
+                f.__doc__)
+    else:
+        raise TypeError("expected type-decorated one-argument function, found " + f.__name__)
+
+
 def pipe_two(oper1: Operation, oper2: Operation) -> Operation:
-    if oper1.valtype == oper2.argtype:
+    if (t1 := oper1.valtype) == (t2 := oper2.argtype):
         return Operation(
             lambda x: oper2(oper1(x)),
             oper1.argtype,
             oper2.valtype,
+            oper1.name + ' | ' + oper2.name,
             '\n'.join([oper1.doc, 'then' + oper2.doc])
             )
     else:
-        raise TypeError("operation types do not match in pipe")
+        raise TypeError(' '.join(
+            ['output type', str(t1), 'of', oper1.name,
+                 'does not match input type', str(t2), 'of', oper2.name]))
 
+    
 def pipe(opers: list[Operation]) -> Operation:
     oper = opers[0]
     for oper2 in opers[1:]:
         oper = pipe_two(oper, oper2)
     return oper
     
-    
 
-def str2wordline(line: str) -> WordLine:
-    "read a string as a WordLine, fail if not valid"
-    return read_wordline(line)
-
-s2w = Operation(
-    str2wordline,
-    str,
-    WordLine,
-    "read a string as a WordLine, fail if not valid"
-    )
-
-w2lemma = Operation(
-    lambda w: w.LEMMA,
-    WordLine,
-    str,
-    "return the LEMMA"
-    )
-
-
+@operation
 def strs2wordlines(lines: Iterable[str]) -> Iterable[WordLine]:
     "read a sequence of strings as WordLines, ignoring failed ones"
     for line in lines:
         try:
-            word = str2wordline(line)
+            word = read_wordline(line)
             yield word
         except:
             pass
+
+
+@operation
+def lines2deptrees(lines: Iterable[str]) -> Iterable[DepTree]:
+    "convert a list of lines into a deptree"
+    comms = []
+    nodes = []
+    for line in lines:
+        if line.startswith('#'):
+            comms.append(line.strip())
+        elif line.strip():
+            t = read_wordline(line)
+            nodes.append(t)
+        else:
+            dt = build_deptree(nodes)
+            dt.comments = comms
+            yield dt
+            comms = []
+            nodes = []
+
+
+@operation
+def deptrees2wordlines(trees: Iterable[DepTree]) -> Iterable[list[WordLine]]:
+    "convert a stream of deptrees to a list of lists of wordlines"
+    for tree in trees:
+        yield tree.wordlines()
+
+
+@operation
+def wordlines2sentences(wordliness: Iterable[list[WordLine]]) -> Iterable[str]:
+    "extract sentences from a stream of lists of wordlines, using the FORM fields"
+    for wordlines in wordliness:
+        yield ' '.join([word.FORM for word in wordlines])
+
+
+def word_match(s: str) -> Operation:
+    patt = parse_pattern(s)
+    return Operation (
+        lambda ws: (w for w in ws if match_wordline(patt, w)),
+        Iterable[WordLine],
+        Iterable[WordLine],
+        'word_match',
+        'pattern matching with wordlines, yielding the ones that match'
+        )
         
 
-def wordlines2deptree(wordlines: list[WordLine]) -> DepTree:
-    "convert a list of wordlines into a deptree"
-    return build_deptree(wordlines)
-
-
-def deptree2wordlines(tree: DepTree) -> list[WordLine]:
-    "convert a deptree to a list of wordlines"
-    return tree.wordlines()
-
-
-def wordlines2sentence(wordlines: list[WordLine]) -> str:
-    "extract a sentence from a list of wordlines, using the FORM fields"
-    return ' '.join([word.FORM for word in self.wordlines()])
-
-
-
+def subtree_match(s: str) -> Operation:
+    patt = parse_pattern(s)
+    def matcht(ts):
+        for tr in ts:
+            for t in matches_in_deptree(patt, tr):
+                yield t
+    return Operation (
+        matcht,
+        Iterable[DepTree],
+        Iterable[DepTree],
+        'subtree_match',
+        'pattern matching with trees and subtrees, yielding the ones that match'
+        )
+        
 if __name__ == '__main__':
-
-    example = "1	This	this	DET	DT	Number=Sing|PronType=Dem	4	det	4:det	_"
-    print(pipe([s2w, s2w])(example))
+    oper = lines2deptrees
+    oper = pipe([lines2deptrees, deptrees2wordlines, wordlines2sentences])
+    oper = pipe([lines2deptrees, subtree_match('DEPREL nsubj'), deptrees2wordlines, wordlines2sentences])
+    for t in oper(sys.stdin):
+        print(t)
+        print()
 
