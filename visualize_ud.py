38dad70d9176a86b7e785f26209f46ca0649272f
Arianna Masciolini <uzkamascio@gmail.com>
2024-03-16 14:54:49 +0100

    correctly render word forms

--- visualize_ud.py
+++ visualize_ud.py
@@ -2,45 +2,54 @@ import sys
 from argparse import ArgumentParser, ArgumentTypeError
 from itertools import chain, repeat, islice
 
-import drawsvg as draw
+from drawsvg import *
 
 from trees import read_wordlines
 
-# measures
+# default measures
 SPACE_LEN = 10
 DEFAULT_WORD_LEN = 20
 CHAR_LEN = 1.8
+FONT_SIZE = 16
+SCALE = 5
 
 # stores CoNLL-U sentence info to be visualized (corresponds to Haskell's Dep)
 # NOTE: all positions are real positions and not token IDs, hence the -1s
 class VisualStanza:
   def __init__(self,stanza):
-    ls = stanza.split("\n")
+    lines = stanza.split("\n")
 
     # parse stanza as list of wordlines, but ignore tokens whose ID is not 
     # an int (floats & ranges)
-    wls = [wl for wl in read_wordlines(ls) if wl.ID.isdigit()]
+    wordlines = [wl for wl in read_wordlines(lines) if wl.ID.isdigit()]
 
-    # dictionary {token position: word token length}
-    # the "token length" is based on the longest between POS and FORM
-    self.token_lens = {
-      i: CHAR_LEN * max(0, len(wl.FORM), len(wl.POS)) 
-      for (i,wl) in enumerate(wls)
-    }
-
-    # list of word forms paired with their POS tag: [(word form, UPOS tag)]
-    self.tokens = [(wl.FORM, wl.POS) for wl in wls] 
+    # list of word tokens (dicts with a form and a pos)
+    self.tokens = [({"form": wl.FORM, "pos": wl.POS}) for wl in wordlines] 
       
     # list of dependency relations: [((from,to), label)]
+    # note: might have to remove root from here to match Haskell program
     self.deprels = [
-      ((int(wl.ID) - 1, int(wl.HEAD) - 1), wl.DEPREL) for wl in wls
+      ((int(wl.ID) - 1, int(wl.HEAD) - 1), wl.DEPREL) for wl in wordlines
       ]
 
     # root word position
-    self.root = int([wl.ID for wl in wls if wl.HEAD == "0"][0]) - 1
+    self.root = int([wl.ID for wl in wordlines if wl.HEAD == "0"][0]) - 1
   
-  def word_width(self, i): 
-    return 100 * (self.token_lens[i] / DEFAULT_WORD_LEN) + SPACE_LEN
+  def word_length(self, i): # cf. wordLength
+    return CHAR_LEN * max(
+      0, 
+      len(self.tokens[i]["form"]), 
+      len(self.tokens[i]["pos"]))
+  
+  def normalized_word_length(self, i): # cf. rwld
+    return self.word_length(i) / DEFAULT_WORD_LEN
+
+  def word_size(self, i): # cf. wsize
+    return 100 * self.normalized_word_length(i) + SPACE_LEN
+  
+  # start x coordinate of i-th word, cf. wpos
+  def word_xpos(self, i): 
+    return sum([self.word_size(j) for j in range(i)])
   
   def arcs(self):
     return [(min(f,t), max(f,t)) for ((f,t),_) in self.deprels]
@@ -60,11 +69,19 @@ class VisualStanza:
     return self.depth(min(f,t), max(f,t)) + 1
   
   def to_svg(self):
-    words_width = sum([self.word_width(i) for i in range(len(self.tokens))])
+    words_width = sum([self.word_size(i) for i in range(len(self.tokens))])
     spaces_width = SPACE_LEN * (len(self.tokens) - 1)
     w = words_width + spaces_width
     h = 50 + 20 * max([0] + [self.arc_height(f,t) for (f,t) in self.arcs()])
-    print(w,h)
+    svg = Drawing(w,h, origin=(0,0)) # might still need to alter w and h
+    
+    # draw words (WIP)
+    for (i,token) in enumerate(self.tokens):
+      x = self.word_xpos(i)
+      y = 100
+      svg.append(Text(token["form"], FONT_SIZE, x=x, y=y))
+    svg.save_html("example.html")
+
 
 if __name__ == "__main__":
   intxt = sys.stdin.read()
