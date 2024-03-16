988700b7c2e64921b7fe8fa6c5b9fe50ccc16842
Arianna Masciolini <uzkamascio@gmail.com>
2024-03-16 19:58:49 +0100

    finally arcs (but no arrows and labels yet)

--- visualize_ud.py
+++ visualize_ud.py
@@ -7,12 +7,13 @@ from drawsvg import *
 from trees import read_wordlines
 
 # default measures
-SPACE_LEN = 15
+SPACE_LEN = 10
 DEFAULT_WORD_LEN = 20
 CHAR_LEN = 1.8
 NORMAL_TEXT_SIZE = 16
 TINY_TEXT_SIZE = 10
 SCALE = 5
+ARC_BASE_YPOS = 30
 
 # stores CoNLL-U sentence info to be visualized (corresponds to Haskell's Dep)
 # NOTE: all positions are real positions and not token IDs, hence the -1s
@@ -42,46 +43,74 @@ class VisualStanza:
       len(self.tokens[i]["form"]), 
       len(self.tokens[i]["pos"]))
   
-  def normalized_word_length(self, i): # cf. rwld
+  def relative_word_length(self, i): # cf. rwld
     return self.word_length(i) / DEFAULT_WORD_LEN
 
   def word_size(self, i): # cf. wsize
-    return 100 * self.normalized_word_length(i) + SPACE_LEN
+    return 100 * self.relative_word_length(i) + SPACE_LEN
   
   # start x coordinate of i-th word, cf. wpos
   def word_xpos(self, i): 
     return sum([self.word_size(j) for j in range(i)])
   
-  def arcs(self):
-    return [(min(f,t), max(f,t)) for ((f,t),_) in self.deprels]
+  # distance between two words x and y
+  def word_dist(self, x, y):
+    return sum([self.word_size(i) for i in range(min(x,y), max(x,y))])
   
-  # depth of the link from f to t
-  def depth(self,f,t):
-    # projective arcs "under" (f,t)?
+  def arcs(self):
+    return [(min(src,trg), max(src,trg)) for ((src,trg),_) in self.deprels]
+  
+  # depth of the link from src to trg
+  def depth(self,src,trg):
+    # projective arcs "under" (src,trg)?
     sub_arcs = [(x,y) for (x,y) in self.arcs() 
-                if (f < x and y <= t) or (f == x and y < t)]
+                if (src < x and y <= trg) or (src == x and y < trg)]
 
     if sub_arcs:
       return 1 + max([0] + [self.depth(x,y) for (x,y) in sub_arcs])
     return 0
 
-  # abs height of the arc between nodes with positions f (from) and t (to)?
-  def arc_height(self, f, t):
-    return self.depth(min(f,t), max(f,t)) + 1
+  # abs height of the arc between nodes with positions src and trg?
+  def arc_height(self, src, trg): # cf. aheight
+    return self.depth(min(src,trg), max(src,trg)) + 1
   
   def to_svg(self):
     words_width = sum([self.word_size(i) for i in range(len(self.tokens))])
     spaces_width = SPACE_LEN * (len(self.tokens) - 1)
-    w = words_width + spaces_width
-    h = 50 + 20 * max([0] + [self.arc_height(f,t) for (f,t) in self.arcs()])
-    svg = Drawing(w,h, origin=(0,0)) # might still need to alter w and h
+    tot_w = words_width + spaces_width
+    tot_h = 50 + 20 * max([0] + [self.arc_height(src,trg) for (src,trg) in self.arcs()])
+    svg = Drawing(tot_w,tot_h, origin=(0,0))
     
-    # draw tokens
+    # draw tokens (froms + pos tags)
     for (i,token) in enumerate(self.tokens):
       x = self.word_xpos(i)
-      y = h
+      y = tot_h
       svg.append(Text(token["form"], NORMAL_TEXT_SIZE, x=x, y=y))
-      svg.append(Text(token["pos"], TINY_TEXT_SIZE, x=x, y=h-15))
+      svg.append(Text(token["pos"], TINY_TEXT_SIZE, x=x, y=tot_h-15))
+
+    # draw deprels (arcs + labels)
+    for ((src,trg),label) in self.deprels:
+      dxy = self.word_dist(src, trg)
+      ndxy = 100 * 0.5 * self.arc_height(src,trg)
+      w = dxy - (600 * 0.5) / dxy
+      h = ndxy / (3 * 0.5)
+      r = h / 2
+      x = self.word_xpos(min(src,trg)) + (dxy / 2) + (20 if x < y else 10) # some centering magic
+      y = ARC_BASE_YPOS
+      ycorrect = lambda y: (round(tot_h)) - round(y)
+      x1 = x - w / 2
+      x2 = min(x, (x1 + r))
+      x4 = x + w / 2
+      x3 = max(x, (x4 - r))
+      y1 = ycorrect(y)
+      y2 = ycorrect(y + r)
+      path = Path(stroke='black', fill='none')
+      path.M(x1, y1)
+      path.Q(x1, y2, x2, y2)
+      path.L(x3,y2)
+      path.Q(x4, y2, x4, y1)
+      svg.append(path)
+      print(x1,y1) # TODO: rm
     svg.save_html("example.html")
 
 
