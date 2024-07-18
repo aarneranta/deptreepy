fb2bed063c42496819cf2b57043ea8c073e23453
Aarne Ranta <aarne@chalmers.se>
2024-07-18 19:07:12 +0200

    added Ita and Fin to models

--- udpipe2_models.py
+++ udpipe2_models.py
@@ -1,8 +1,10 @@
 
 model_dict = {
     'Eng': 'english-ewt-ud-2.12-230717',
+    'Fin': 'finnish-tdt-ud-2.12-230717',
     'Fre': 'french-gsd-ud-2.12-230717',
-    'Ger': 'german-gsd-ud-2.12-230717'
+    'Ger': 'german-gsd-ud-2.12-230717',
+    'Ita': 'italian-isdt-ud-2.12-230717'
     }
 ## TODO: complete this from
 # https://lindat.mff.cuni.cz/services/udpipe/api/models
