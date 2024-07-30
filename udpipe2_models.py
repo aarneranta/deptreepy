858b9d2446562befe720eae56e229fabe002f668
Aarne Ranta <aarne@chalmers.se>
2024-07-30 17:14:19 +0200

    more udpipe2 models

--- udpipe2_models.py
+++ udpipe2_models.py
@@ -1,10 +1,16 @@
 
 model_dict = {
+    'Bul': 'bulgarian-btb-ud-2.12-230717',
+    'Cze': 'czech-pdt-ud-2.12-230717',
     'Eng': 'english-ewt-ud-2.12-230717',
     'Fin': 'finnish-tdt-ud-2.12-230717',
     'Fre': 'french-gsd-ud-2.12-230717',
     'Ger': 'german-gsd-ud-2.12-230717',
-    'Ita': 'italian-isdt-ud-2.12-230717'
+    'Hrv': 'croatian-set-ud-2.12-230717',
+    'Ita': 'italian-isdt-ud-2.12-230717',
+    'Por': 'portuguese-bosque-ud-2.12-230717',
+    'Spa': 'spanish-gsd-ud-2.12-230717',
+    'Swe': 'swedish-talbanken-ud-2.12-230717'
     }
 ## TODO: complete this from
 # https://lindat.mff.cuni.cz/services/udpipe/api/models
